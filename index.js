70fc0e71365c5a7a1eb20143006dfd1b51be0090
Feng-Peng <1559139360@qq.com>
2019-12-09 20:25:42 +0800

    读取cookie

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ const express = require('express');
 const globalConfig = require('./conf');
 const app = new express();
 const loader = require('./loader');
+const cookie = require('cookie-parser');
 app.use(express.static(globalConfig.page_path));
+app.use(cookie());
 
 app.get('/api/*', function (request, response, next) {
-    response.redirect("/login.html");
-    // next();
+    if (request.cookies.id) {
+        next();
+    } else {
+        response.redirect("/login.html");
+    }
 })
 
 loader.init(app);
